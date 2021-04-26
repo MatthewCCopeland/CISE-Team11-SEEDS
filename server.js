abea4e670c27b406dd59a41b2ec6d4f46b715bfb
MatthewCCopeland <pry2051@aut.ac.nz>
2021-04-26 17:42:04 +1200

    Initial Routes and Controllers setup

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-// app.js
-
+// server.js
+require('dotenv').config({path: "./config.env"});
 const express = require('express');
 const connectDB = require('./config/db');
 
 const app = express();
+
+app.use(express.json());
+
+//Redirect any incoming traffic on api/auth to the authentication router
+app.use("/api/auth", require("./routes/auth"));
 
 // Connect Database
 connectDB();
